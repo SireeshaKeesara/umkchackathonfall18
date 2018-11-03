ebf89bb2959cc2429e0c2cab2961b37f990fb96b
benamreview <dhh3hb@mail.umkc.edu>
2018-11-03 16:53:01 -0500

    Initial Commit

---
+++ SourceCode/app/src/main/java/diet/smsd/hackathon/smsddiet/MainActivity.java
@@ -0,0 +1,13 @@
+package diet.smsd.hackathon.smsddiet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

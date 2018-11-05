438309882b7976d3ceeebb55f54a80b9db1433d6
benamreview <dhh3hb@mail.umkc.edu>
2018-11-04 22:07:30 -0600

    Exported APK

--- SourceCode/app/build/generated/source/r/debug/com/google/android/gms/base/R.java
+++ SourceCode/app/build/generated/source/r/debug/com/google/android/gms/base/R.java
@@ -56,11 +56,11 @@ public final class R {
         public static final int adjust_width = 0x7f08001c;
         public static final int auto = 0x7f080022;
         public static final int dark = 0x7f080038;
-        public static final int icon_only = 0x7f080051;
-        public static final int light = 0x7f080058;
-        public static final int none = 0x7f080062;
-        public static final int standard = 0x7f08008f;
-        public static final int wide = 0x7f0800a8;
+        public static final int icon_only = 0x7f080050;
+        public static final int light = 0x7f080057;
+        public static final int none = 0x7f080061;
+        public static final int standard = 0x7f08008d;
+        public static final int wide = 0x7f0800a6;
     }
     public static final class string {
         public static final int common_google_play_services_enable_button = 0x7f0c0028;
